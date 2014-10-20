3063f79666b7f147cc0173f91153448f675a58fa
fatwa <fafa.ravencruze@yahoo.com>
2014-10-21 00:59:33 +0700

    pertama

---
+++ js/game.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+game.init();
+});
+
+var game = {
+// Start initializing objects, preloading assets and display start screen
+init: function(){
+// Hide all game layers and display the start screen
+$('.gamelayer').hide();
+$('#gamestartscreen').show();
+//Get handler for game canvas and context
+game.canvas = $('#gamecanvas')[0];
+game.context = game.canvas.getContext('2d');
+},
+};
